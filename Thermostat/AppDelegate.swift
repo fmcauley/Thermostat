5b63761374d7e535cb825074874055cd1c52ca0d
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-23 15:55:26 -0400

    Initial Commit

---
+++ Thermostat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Thermostat
+//
+//  Created by MLS Discovery on 7/23/16.
+//  Copyright © 2016 SoftwareSoFast. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
