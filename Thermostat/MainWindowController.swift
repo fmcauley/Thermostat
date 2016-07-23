dd5bf097dfd400ac11da1b3f5e2015e50acd363f
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-23 16:02:42 -0400

    Initial commit

---
+++ Thermostat/MainWindowController.swift
@@ -0,0 +1,21 @@
+//
+//  MainWindowController.swift
+//  Thermostat
+//
+//  Created by MLS Discovery on 7/23/16.
+//  Copyright © 2016 SoftwareSoFast. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+    
+}
